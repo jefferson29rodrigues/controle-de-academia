f6277b5c19498f6f1b66669d0ad0c261f5947034
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-08-14 15:17:04 -0300

    routes.js servindo about.njk

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get('/', function(req, res) {
-    return res.send('Hello World!')
+    return res.render('about')
 });
 
 module.exports = routes;
