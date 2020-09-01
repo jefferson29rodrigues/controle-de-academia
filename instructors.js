270aa89221b618f1e1986fac2e8a32af8f6c0ca3
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-01 16:09:27 -0300

    página guardando dados no arquivo data.json e redirecionando!

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const data = require("./data.json")
 
 // create
 exports.post = function(req, res) {
@@ -12,13 +13,16 @@ exports.post = function(req, res) {
        }
    }
 
-   fs.writeFile("data.json", JSON.stringify(req.body), function() {
+   // na 1° [] -> [{...}] na 2° [{...}] -> [{...}, {...}] 
+   data.instructors.push(req.body) //  na 3° [{...}, {...}] -> [{...}, {...}, {...}]
+
+   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
        if (err) return res.send("write file error!")
 
        return res.redirect("/instructors")
    });
 
-    return res.send(req.body)
+    //return res.send(req.body)
 }
 
 // update
