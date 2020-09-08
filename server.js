eb03fbb599a1ed1600cf0dee57b026c00f028f63
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-08 00:38:49 -0300

    criado o arquivo show.njk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const routes = require('./routes')
+const routes = require('./routes');
 const server = express();
 
 server.use(express.urlencoded({ extended: true }))
