a27dad3dc009e70ec98e8577335d7071b03df945
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-10 18:19:04 -0300

    data formatada

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const data = require("./data.json")
+const { age } = require('./utils')
 
 //show
 exports.show = function(req, res) {
@@ -15,27 +16,13 @@ exports.show = function(req, res) {
 
     if (!foundInstructor) return res.send("Instructor not found!")
 
-    function age(timestamp) {
-        const today = new Date()
-        const birthDate = new Date(timestamp)
-
-        let age = today.getFullYear() - birthDate.getFullYear()
-        const month = today.getMonth() -  birthDate.getMonth()
-
-        if (month < 0 || 
-            month == 0 &&
-            today.getDate() <= birthDate.getDate()) {
-                age = age - 1
-            }
-
-        return age
-    }
+    
 
     const instructor = {
       ...foundInstructor,
       age: age(foundInstructor.birth),
       services: foundInstructor.services.split(","),
-      created_at: "",
+      created_at: new Intl.DateTimeFormat("en-GB").format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", { instructor })
