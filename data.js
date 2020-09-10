4bd3b289c25f67948e06602b86d92c986092623f
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-10 17:29:12 -0300

    sistemas calculando e mostrando idade

--- data.js
+++ data.js
@@ -38,3 +38,26 @@ module.exports = [
         price: "Free"
     }
 ];
+
+/******************************************************************
+
+function age(timestamp) {
+    const today = new Date()
+    const birthDate = new Date(timestamp)
+
+    // 2019 - 1984 = 35
+    let age = getFullYear() - birthDate.getFullYear()
+
+
+    const month = today.getMonth() - birthDate.getMonth()
+    
+    // 11 - 12 = -1
+    // 11 - 11 =  0
+    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+        age = age - 1
+    }
+
+    return age
+}
+
+*/
